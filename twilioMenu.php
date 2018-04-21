84648a6ed1584c5cfd867d82eb7b0247dfc156f9
iguanaus <johnpeurifoy@yahoo.com>
2018-04-20 21:36:40 -0400

    new

--- twilioMenu.php
+++ twilioMenu.php
@@ -46,6 +46,35 @@ function noResponseMsg($order_id){
 
     $orderRecord = getSID();
 
+    // there should only be one order per customer, therefore wpdb update should only return 1
+    if (0 != $wpdb->update($STAT, array($STAT_etime => $time, $STAT_ack => 'Y', $STAT_ctime => date('Y-m-d H:i:s')), array($STAT_id => $orderRecord))){
+        // send confirmtion message to customer once order is complete
+        $client = new Client($TWIL_ACC_SID, $TWIL_TOKEN);
+
+        $sqlMes = $SQL_MSG2.$orderRecord.'"';
+        $result = $wpdb->get_results($sqlMes, "ARRAY_A");
+            
+        try {
+            $message = $client->messages->create($result[0]["cus_num"], array('From' => $TWIL_NUM,'Body' => "Hey ".$result[0]["cus_name"].", Hungry here. Your order was confirmed by ".$result[0]["res_name"]." and will be ready ".$message.". Your order id is ".$orderRecord."."));
+            logTwil("Order confirmation message sent: " . $message->sid);
+        } 
+        catch (Exception $e) {
+            logTwil("Order confirmation message error: " . $e->getMessage());
+        }
+
+        //send message to custome of confirmation
+        header('content-type: text/xml');
+        $output = new TwiML();
+        $output->say('Thank you for confirming order. '.$result[0]["cus_name"].' will be expecting their order in '.$time.' minutes. Have a nice day.',['voice' => 'alice']);
+        echo $output;
+    }
+    else{
+        logTwil('WPDB access confirmation message error: 0 records affected or returned flase.');
+    }
+
+
+    /*
+
     logTwil("Order record: " . $orderRecord);
 	
 	$client = new Client($TWIL_ACC_SID, $TWIL_TOKEN);
@@ -62,6 +91,7 @@ function noResponseMsg($order_id){
 		logTwil("Restaurant no response message error: " . $e->getMessage());
 		#logTwil("Restaurant no response message error texting: " . $result[0]["cus_num"]);
 	}
+	*/
 }
 
 
